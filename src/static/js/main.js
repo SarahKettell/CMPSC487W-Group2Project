602e546feb850cf6773cce4bb33d3e94f1fcc953
SarahKettell <sakettell@gmail.com>
2020-10-27 20:56:19 -0400

    Updated with some db stuff

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,13 +1,15 @@
 // client side functions
+
+/*******************************************************************
+* Following functions deal with GETTING and DISPLAYING 
+* info from the Database
+********************************************************************/
 // async function to retreive the json output from the database
 const getJsonData = async (textBox) => {
 	// get database "items"
 	const response = await fetch('http://localhost:3000/items');
 	const myJson = await response.json(); 
-	// Create a text element with the string version of the JSON
-	let textInput = await document.createTextNode(JSON.stringify(myJson));
-	// Add that to the div in the main webpage
-	textBox.appendChild(textInput);
+	displayMenuItems(textBox, myJson);
 }
 
 // called from webpage, gets the data and it in the location given
@@ -17,20 +19,40 @@ function getDatafromDB(elementID){
     getJsonData(location);
 }
 
+// Converts the jsonData into menu item text
+// appends it to the element referenced
+function displayMenuItems(textBox, jsonData){
+  let menuItems = [];
+  for(let i = 0; i < jsonData.length; i++){
+    menuItems.push(jsonData[i]);
+  }
+
+  let textInput = document.createTextNode(JSON.stringify(jsonData));
+  textBox.appendChild(textInput);
+}
+
 
 // adds the data to the database
 const addDatatoDB = async (inputData) => {
 	// creates the item object
-	const testItem = {
-        id: 6,
-        name: inputData,
-        completed: false,
+	const newMenuItem = {
+        id: 0,
+        pizza_name: 'Plain Cheese',
+        crust: 'thin',
+        sauce: 'white',
+        cheese: 'three cheese blend',
+        toppings: 'none',
+        sm_price: '6.00',
+        med_price: '8.00',
+        lg_price: '10.00',
+        xlg_price: '12.00',
+        description: 'It has cheese only.'
     };
 
     // creates a post request, which is defined in the src/index.js file to call "addItem.js"
   	const response = await fetch('http://localhost:3000/items', {
     	method: 'POST',
-    	body: JSON.stringify(testItem), // string or object
+    	body: JSON.stringify(newMenuItem), // string or object
     	headers: {
       		'Content-Type': 'application/json'
     	}

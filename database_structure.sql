93fa7bfd7ffb1fd652e3599b9c92f6a378dad8a5
SarahKettell <sakettell@gmail.com>
2020-11-08 15:54:32 -0500

    Edits

--- database_structure.sql
+++ database_structure.sql
@@ -15,11 +15,54 @@ create table menu_items
 		primary key(item_id)
 );
 
-insert into menu_items 
-	values('1', 'Meat Lovers', 
-		'thick', 'red', 'mozzarella', 
-		'pepperoni, sausage, bacon', 
-		'8.00', '10.00','12.00','16.00',
-		'It has meat.'
+insert into menu_items values('1', 'Meat Lovers', 'thick', 'red', 'mozzarella', 'pepperoni, sausage, bacon', '8.00', '10.00','12.00','16.00', 'It has all of the meat.');
+insert into menu_items values('2','Veggie Extravaganza', 'thick', 'red', 'cheddar', 'green pepper, mushrooms, ', '8.00', '10.00','12.00','16.00','All of your favorite vegetables combined.');
+
+
+// IN PROGRESS DO NOT USE
+create table orders(
+	order_id varchar(36),
+	customer_id varchar(36),
+	time_placed
+	date_placed
+	time_scheduled
+	date_scheduled
+	type
+	notes
+	payment_type
+	sub_total_price
+	tax_price
+	tip_price
+	deliver_fee
+	total_price
+	checked_out
+	completed
+	primary key(item_id)
+);
+
+create table order_items(
+	order_item_id
+	order_id
+	crust
+	size
+	price
+	notes
+);
+
+create table order_item_toppings(
+	order_item_id
+	topping_id
+);
+
+create table menu_item_toppings(
+	menu_item_id
+	topping_id
+);
+
+create table toppings(
+	topping_id
+	topping_name
+	in_stock
+	current_topping
 );
 
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -40,6 +40,21 @@ function displayMenuItems(textBox, jsonData){
     menuValues[i] = Object.values(jsonData[i]);
   }
 
+  // // iterate over each key value pair to output the strings
+  // for(let i = 0; i < menuValues.length; i++){
+  //   let newUL = document.createElement("ul");
+  //   newUL.classList.add("menu-item-container");
+  //   let newItem = document.createElement("li");
+  //   newItem.classList.add("row");
+  //   newItem.classList.add("menu-item-box");
+  //   for(let j = 1; j < menuKeys.length; j++){
+  //     let textInput = document.createTextNode(menuKeys[j] + ": " + menuValues[i][j]);
+  //     newItem.appendChild(textInput);
+  //     newDiv.appendChild(newItem);
+  //   }
+  //   textBox.appendChild(newDiv);
+  // }
+
   // create a new div element to add contents to
   const newDiv = document.createElement("div");
 
@@ -47,15 +62,78 @@ function displayMenuItems(textBox, jsonData){
   for(let i = 0; i < menuValues.length; i++){
     let newUL = document.createElement("ul");
     newUL.classList.add("menu-item-container");
-    let newItem = document.createElement("li");
+    let newItem = document.createElement("div");
     newItem.classList.add("row");
-    newItem.classList.add("menu-item-box");
-    for(let j = 1; j < menuKeys.length; j++){
-      let textInput = document.createTextNode(menuKeys[j] + ": " + menuValues[i][j]);
-      newItem.appendChild(textInput);
-      newDiv.appendChild(newItem);
-    }
-    textBox.appendChild(newDiv);
+    newItem.classList.add("main-menu-item");
+    //newItem.classList.add("menu-item-box");
+    let newHeader = document.createElement("div");
+
+    // image
+    let imageDiv = document.createElement("div");
+    imageDiv.classList.add("image-div");
+    newItem.appendChild(imageDiv);
+
+
+    let contentDiv = document.createElement("div");
+    contentDiv.classList.add("content-div");
+
+    // title
+    let newTitle = document.createElement("h2");
+    newTitle.appendChild(document.createTextNode(jsonData[i].pizza_name));
+    newHeader.appendChild(newTitle);
+    contentDiv.appendChild(newHeader);
+
+    // List of pizza style options
+    let styleUL = document.createElement("ul");
+    let crustLI = document.createElement("li");
+    crustLI.appendChild(document.createTextNode(jsonData[i].crust));
+    let sauceLI = document.createElement("li");
+    sauceLI.appendChild(document.createTextNode(jsonData[i].sauce));
+    let cheeseLI = document.createElement("li");
+    cheeseLI.appendChild(document.createTextNode(jsonData[i].cheese));
+    let toppingsLI = document.createElement("li");
+    toppingsLI.appendChild(document.createTextNode(jsonData[i].toppings));
+    styleUL.appendChild(crustLI);
+    styleUL.appendChild(sauceLI);
+    styleUL.appendChild(cheeseLI);
+    styleUL.appendChild(toppingsLI);
+    contentDiv.appendChild(styleUL);
+
+    // list of prices for item
+    let priceUL = document.createElement("ul");
+    let smPrice = document.createElement("li");
+    smPrice.appendChild(document.createTextNode("Small Size: $" + jsonData[i].sm_price));
+    let medPrice = document.createElement("li");
+    medPrice.appendChild(document.createTextNode("Medium Size: $" + jsonData[i].med_price));
+    let lgPrice = document.createElement("li");
+    lgPrice.appendChild(document.createTextNode("Large Size: $" + jsonData[i].lg_price));
+    let xlgPrice = document.createElement("li");
+    xlgPrice.appendChild(document.createTextNode("Extra Large Size: $" + jsonData[i].xlg_price));
+    priceUL.appendChild(smPrice);
+    priceUL.appendChild(medPrice);
+    priceUL.appendChild(lgPrice);
+    priceUL.appendChild(xlgPrice);
+    contentDiv.appendChild(priceUL);
+
+    // list the description for the item
+    let descUL = document.createElement("ul");
+    let descriptionLI = document.createElement("li");
+    descriptionLI.appendChild(document.createTextNode(jsonData[i].description));
+    descUL.appendChild(descriptionLI);
+    contentDiv.appendChild(descUL);
+
+    // delete item link
+    let orderLink = document.createElement("a");
+    let orderLinkText = document.createTextNode("Add to Cart");
+    orderLink.title = "Delete Menu Item";
+    orderLink.href = "";
+    orderLink.id = jsonData[i].item_id;
+    orderLink.classList.add("delete-button");
+    orderLink.appendChild(orderLinkText);
+    contentDiv.appendChild(orderLink);
+
+    newItem.appendChild(contentDiv);
+    textBox.appendChild(newItem);
   }
 }
 
